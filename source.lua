28ae8777dfaaf8d50840a7302cf187505a67d379
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-07 15:54:39 -0400

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,20 @@
+-- Loadstring
+
+
+
+-- Windows --
+
+
+
+-- Tabs --
+
+-- Game Checker --
+local GamesSupported = {
+    ["6872265039"] = true, --Bedwars Lobby
+    ["8560631822"] = true, --Bedwars Solos, Duels 2v2
+    ["6872274481"] = true, --Bedwars Doubles, Squads, LuckyBlock Squads, LuckyBlock Doubles, SkyWars Doubles
+    ["8444591321"] = true, --Bedwars 30vs30
+    ["855499080"] = true, --Skywars
+}
+
+-- Code --
