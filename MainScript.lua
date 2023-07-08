1c4a88e9e6ceb1648a05eaf4cdeb7cd079446060
Sail100 <85443860+Sail100@users.noreply.github.com>
2023-07-08 16:36:19 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -4,36 +4,50 @@ local isfile = isfile or function(path)
 	return suc and res ~= nil 
 end
 
+local kavo
+if isfile("BedWarsCode/Libraries/kavo.lua") then
+  kavo = loadstring(readfile("BedWarsCode/Libraries/kavo.lua"))()
+else
+  kavo = loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/Libraries/kavo.lua", true))()
+end
+shared.kavogui = kavo
+local Sections = {}
+shared.SectionsLoaded = Sections
+
 local entityLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/Libraries/entityHandler.lua", true))()
 shared.vapeentity = entityLibrary
-local FunctionsLibrary = loadstring(game:HttpGet("https://github.com/Sail100/BedWarsCode/blob/main/Libraries/FunctionsHandler.lua", true))()
+local FunctionsLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/Libraries/FunctionsHandler.lua", true))()
 shared.funcslib = FunctionsLibrary
 
 local queueteleport = queue_on_teleport
 
 local TeleportString = [[
-  loadstring(game:HttpGet("https://github.com/Sail100/BedWarsCode/blob/main/MainScript.lua", true))
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/MainScript.lua", true))
 ]]
 
 queueteleport(TeleportString)
 
-if isfolder("BedWarsScript") == false then
-	makefolder("BedWarsScript")
+if isfolder("BedWarsCode") == false then
+	makefolder("BedWarsCode")
 end
 
-if isfolder("BedwarsScript/assets") == false then
-	makefolder("BedWarsScript/assets")
+if isfolder("BedWarsCode/assets") == false then
+	makefolder("BedwarsCode/assets")
 end
 
-if isfolder("BedwarsScript/Profiles") == false then
-  makefolder("BedwarsScript/Profiles")
+if isfolder("BedWarsCode/Profiles") == false then
+  makefolder("BedWarsCode/Profiles")
 end
-if isfolder("BedWarsScript/CustomModules") == false then
-	makefolder("BedWarsScript/CustomModules")
+local AnyGame = [[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua",
+true))()
+]]
+if isfolder("BedwarsCode/CustomModules") == false then
+	makefolder("BedwarsCode/CustomModules")
 end
 
 function MainLoaded()
-  local customModuleURL = "https://github.com/Sail100/BedWarsCode/tree/main/CustomModules/"..game.PlaceId..".lua"
+  local customModuleURL = "https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua"
   local customModuleScript = game:HttpGet(customModuleURL, true)
   if customModuleScript then
     local success, error = pcall(function()
@@ -41,17 +55,21 @@ function MainLoaded()
     end)
     if not success then
       warn("Failed To Loaded Modules: " .. tostring(error))
+      loadstring(AnyGame)()
     end
   else
+    loadstring(AnyGame)()
   end
 end
 
 --[[
 function MainLoaded()
-  if game:HttpGet("https://github.com/Sail100/BedWarsCode/tree/main/CustomModules/"..game.PlaceId..".lua") then
-  	loadstring(game:HttpGet("https://github.com/Sail100/BedWarsCode/tree/main/CustomModules/"..game.PlaceId..".lua", true))()
-  elseif isfile("BedWarsScript/CustomModules/"..game.PlaceId..".lua") then
-    loadstring(readfile("BedWarsScript/CustomModules/"..game.PlaceId..".lua"))()
+  if game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua") then
+  	loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/CustomModules/"..game.PlaceId..".lua", true))()
+  elseif isfile("Nightbed/CustomModules/"..game.PlaceId..".lua") then
+    loadstring(readfile("Nightbed/CustomModules/"..game.PlaceId..".lua"))()
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/NightbedForRoblox/main/AnyGame.lua"))()
   end
 end
 --]]
